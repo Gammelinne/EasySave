d02cc9fab42575fc0d839d78b82bae7800e09d02
JlIlIllIlIIlIll <115548989+JlIlIllIlIIlIll@users.noreply.github.com>
2022-11-24 23:21:57 +0100

    Improvement V2

--- Livrable 1/Program.cs
+++ Livrable 1/Program.cs
@@ -19,16 +19,11 @@ namespace Livrable_1
         {
             Console.WriteLine("Please enter what you want to do / Veuillez entrer ce que vous voulez faire");
             Console.WriteLine("");
-            Console.WriteLine("1. Transfer a folder / 1. Transférer un folder");
-            Console.WriteLine("2. get log of the day / 2. obtenir le log du jour");
-            Console.WriteLine("3. get state of the day / 3. obtenir l'etat des sauvegardes du jour");
-            Console.WriteLine("4. exit / 4. quitter");
+            Console.WriteLine("1. Create a Backup / Créer une sauvegarde");
+            Console.WriteLine("2. Get log of the day / Obtenir le log du jour");
+            Console.WriteLine("3. Get state of the day / Obtenir l'etat des sauvegardes du jour");
+            Console.WriteLine("4. Exit / Quitter");
             Console.WriteLine("");
-
-            /*
-            Log log = new Log("test", "test", "test", 12, 14, DateTime.Now);
-            log.SaveLog();
-            */
 
             string choice = Console.ReadLine();
             Console.Clear();
@@ -43,15 +38,18 @@ namespace Livrable_1
                     Console.WriteLine("Please enter the destination of the file / Veuillez entrer la destination du fichier");
                     string fileDestination = Console.ReadLine();
                     Console.WriteLine("Please enter the type of the file / Veuillez entrer le type du fichier");
-                    Console.WriteLine("1. Complete / 1. Complet");
-                    Console.WriteLine("2. Differentiel / 2. Differentiel");
+                    Console.WriteLine("1. Complete / Complet");
+                    Console.WriteLine("2. Differentiel / Differentiel");
                     string fileType = Console.ReadLine();
-                    if (fileType == "1" || fileType == "2")
+                    while (fileType != "1" && fileType != "2")
                     {
-                        Save save = new Save(name, fileSource, fileDestination, fileType);
-                        save.SaveSave();
+                        Console.WriteLine("Please enter a valid choice / Veuillez entrer un choix valide : ");
+                        fileType = Console.ReadLine();
                     }
+                    Save save = new Save(name, fileSource, fileDestination, fileType);
+                    save.SaveSave();
                     break;
+
                 case "2":
                     try
                     {
@@ -64,6 +62,7 @@ namespace Livrable_1
                         SaveData();
                     }
                     break;
+
                 case "3":
                     try
                     {
@@ -76,9 +75,11 @@ namespace Livrable_1
                         SaveData();
                     }
                     break;
+
                 case "4":
                     Environment.Exit(0);
                     break;
+
                 default:
                     Console.Clear();
                     Console.WriteLine("Please enter a valid choice / Veuillez entrer un choix valide");
