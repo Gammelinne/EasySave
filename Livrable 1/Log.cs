1909832a04b05088ddc2addf9ac12d3a0d332c91
SourcierDeVerite <lefranccharlespro@gmail.com>
2022-11-25 11:36:10 +0100

    Revert "Last commit (Make a valid log json)"
    This reverts commit d059d7aee4765afafb4f739e7a82d2abfe0a032c.

--- Livrable 1/Log.cs
+++ Livrable 1/Log.cs
@@ -11,10 +11,15 @@ namespace Livrable_1
         public string FileSource;
         public string FileDestination;
         public int FileSize;
-        public double FileTransfertTime;
+        public double FileTransfertTime; // in ms.
         public DateTime Time;
 
-        public Log(string name, string fileSource, string fileDestination, int fileSize,  double fileTransfertTime, DateTime time)
+        public Log(string name, 
+            string fileSource, 
+            string fileDestination, 
+            int fileSize, 
+            double fileTransfertTime, 
+            DateTime time)
         {
             Name = name;
             FileSource = fileSource;
@@ -26,33 +31,21 @@ namespace Livrable_1
         
         public void SaveLog()
         {
-            //Check if all directory exist
-            string PathLog = @"C:\EasySave\Log\" + DateTime.Now.ToString("dd-MM-yyyy") + ".json";
-            if (!Directory.Exists(@"C:\EasySave"))
-            {
-                Directory.CreateDirectory(@"C:\EasySave");
-            }
-            if (!Directory.Exists(@"C:\EasySave\Log\"))
-            {
-                Directory.CreateDirectory(@"C:\EasySave\Log\");
-            }
-
-            //Create a valid Json
-            #region
+            string PathLog = @"C:\ProjetCsFT\Log\" + DateTime.Now.ToString("dd-MM-yyyy") + ".json";
+           
             if (!File.Exists(PathLog))
             {
-                File.WriteAllText(PathLog, "[]");
+                StreamWriter newFile = new StreamWriter(PathLog, true);
+                newFile.WriteLine("[]");
+                newFile.Close();
             }
+
             string data = File.ReadAllText(PathLog);
-            data = data.Remove(data.LastIndexOf("]"), 1);
-            if (data.LastIndexOf("}") != -1)
-            {
-                data = data.Insert(data.LastIndexOf("}") + 1, ",\n");
-                File.WriteAllText(PathLog, string.Empty);
-                File.WriteAllText(PathLog, data);
-            }
-            File.WriteAllText(PathLog, data + JsonSerializer.Serialize(GetAll()) + "]");
-            #endregion
+            data = data.Remove(data.Length - 3, 1);
+            File.WriteAllText(PathLog, string.Empty);
+            StreamWriter file = new StreamWriter(PathLog, true);
+            file.WriteLine(data + JsonSerializer.Serialize(GetAll()) + ",\n]");
+            file.Close();
         }
 
         //Get all element of the class and place in a dictonary 
@@ -73,7 +66,7 @@ namespace Livrable_1
         //Open log file and read the content
         public static void ReadLogOfTheDay()
         {
-            string PathLog = @"C:\EasySave\Log\" + DateTime.Now.ToString("dd-MM-yyyy") + ".json";
+            string PathLog = @"C:\ProjetCsFT\Log\" + DateTime.Now.ToString("dd-MM-yyyy") + ".json";
             if (File.Exists(PathLog))
             {
                 string[] lines = File.ReadAllLines(PathLog);
