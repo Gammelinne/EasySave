5ddef6917913311e829479b887e6a35c316d5eb5
JlIlIllIlIIlIll <115548989+JlIlIllIlIIlIll@users.noreply.github.com>
2022-11-27 18:28:28 +0100

    User interface

--- Livrable 2/EasySaveApp/MVVM/ViewModel/MainViewModel.cs
+++ Livrable 2/EasySaveApp/MVVM/ViewModel/MainViewModel.cs
@@ -7,6 +7,43 @@ namespace EasySaveApp.MVVM.ViewModel
 {
     internal class MainViewModel : ObservableObject
     {
-        public MainViewModel() { }
+
+		public RelayCommand HomeViewCommand { get; set; }
+		public RelayCommand ChangeViewCommand { get; set; }
+
+        public HomeViewModel HomeVM { get; set; }
+
+		public ChangeViewModelTest ChangeVM { get; set; }
+
+		private object _currentView;
+
+		public object CurrentView
+		{
+			get { return _currentView; }
+			set 
+			{ 
+			_currentView = value;
+			OnPropertyChanged();
+			}
+		}
+
+
+		public MainViewModel()
+		{
+			HomeVM = new HomeViewModel();
+            ChangeVM = new ChangeViewModelTest();
+
+            CurrentView = HomeVM;
+
+			HomeViewCommand = new RelayCommand(o => 
+			{
+				CurrentView= HomeVM;
+			});
+
+            ChangeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = ChangeVM;
+            });
+        }
     }
 }
