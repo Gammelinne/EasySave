dc3675b4a3264e30d7312499ed9a0b795fa069ae
SourcierDeVerite <lefranccharlespro@gmail.com>
2022-12-02 10:14:07 +0100

    Parse JSON file settings to Dictionnary

--- App.xaml.cs
+++ App.xaml.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Text.Json;
+using System.Diagnostics;
+using System.IO;
 
 namespace EasySaveApp
 {
@@ -13,5 +17,12 @@ namespace EasySaveApp
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+
+            string json = File.ReadAllText(@"C:\Users\lefra\OneDrive - Association Cesi Viacesi mail\CESI\3eme Année\Programmation Système\Projet\EasySave\Livrable 2\Settings\Settings.json");
+            Dictionary<string, string> settings = JsonSerializer.Deserialize<Dictionary<string, string>>(json);
+            MessageBox.Show(settings["TypeOfLog"]);
+        }
     }
 }
