a7e54e1560eaf02b9878f60a84664dacaab65132
SourcierDeVerite <lefranccharlespro@gmail.com>
2022-12-02 16:54:47 +0100

    Add log setting

--- MVVM/ViewModel/SettingViewModel.cs
+++ MVVM/ViewModel/SettingViewModel.cs
@@ -1,10 +1,9 @@
 ﻿using EasySaveApp.Core;
-using EasySaveApp.MVVM.Model;
-using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Text;
+using System.Text.Json;
 using System.Windows;
+
 
 namespace EasySaveApp.MVVM.ViewModel
 {
@@ -14,9 +13,12 @@ namespace EasySaveApp.MVVM.ViewModel
         {
             set
             {
-                string[] lines = File.ReadAllLines("../../../Settings.json");
-                
-                
+                string newValue = value.Replace("System.Windows.Controls.ComboBoxItem\u00A0: ", "");
+                Application.Current.Properties["TypeOfLog"] = newValue;
+                string json = File.ReadAllText("../../../Settings.json");
+                Dictionary<string, string> setting = JsonSerializer.Deserialize<Dictionary<string, string>>(json);
+                setting["TypeOfLog"] = newValue;
+                File.WriteAllText("../../../Settings.json",JsonSerializer.Serialize(setting));
             }
         }
     }
