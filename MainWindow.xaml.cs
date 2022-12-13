b30b4c153eafc9aec9616ebedc162979efd75d17
SourcierDeVerite <lefranccharlespro@gmail.com>
2022-12-13 15:17:28 +0100

    Add DistantManagement project

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Text.Json;
 using System.Windows;
+using System.IO;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading;
 
 namespace EasySaveApp
 {
@@ -18,7 +22,43 @@ namespace EasySaveApp
             Application.Current.Properties["ExtensionToCrypt"] = ".txt .exe";
             Application.Current.Properties["CryptKey"] = "100";
             Application.Current.Properties["Software"] = "vlc notepad";
-            Application.Current.Properties["PriorityFiles"] = ".ben .txt ";
+
+            Socket socket = Connect();
+            Socket client = AllowConnection(socket);
+            SendMessage(client, "Salut1");
+            Thread.Sleep(40);
+            SendMessage(client, "<END>");
+            Disconnect(client);
+
+        }
+
+        private static Socket Connect()
+        {
+            IPHostEntry host = Dns.GetHostEntry("localhost");
+            IPAddress address = host.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(address, 11000);
+            Socket socket = new Socket(address.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            socket.Bind(endPoint);
+            socket.Listen(10);
+            return socket;
+        }
+
+        private static Socket AllowConnection(Socket socket)
+        {
+            Socket acceptedSocket = socket.Accept();
+            return acceptedSocket;
+        }
+
+        private static void SendMessage(Socket client, string message)
+        {
+            byte[] messageByte = Encoding.ASCII.GetBytes(message + "<STOP>");
+            client.Send(messageByte);
+        }
+
+        private static void Disconnect(Socket socket)
+        {
+            socket.Shutdown(SocketShutdown.Both);
+            socket.Close();
         }
 
         private void FrenchButton_Checked(object sender, RoutedEventArgs e)
