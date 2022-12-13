842137d70689ce2192d468c3f7f993707caba341
SourcierDeVerite <lefranccharlespro@gmail.com>
2022-12-13 14:26:28 +0100

    Correction of cryposoft problem

--- MVVM/Model/Save.cs
+++ MVVM/Model/Save.cs
@@ -1,8 +1,9 @@
 ﻿using EasySaveApp.MVVM.ViewModel;
 using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Runtime.CompilerServices;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,6 +19,7 @@ namespace EasySaveApp.MVVM.Model
         private string pathSource;
         private string pathDestination;
         private string saveType;
+        public static bool is_first_save = true;
 
         public string Name { get => name; set => name = value; }
         public string PathSource { get => pathSource; set => pathSource = value; }
@@ -25,15 +27,9 @@ namespace EasySaveApp.MVVM.Model
         public string SaveType { get => saveType; set => saveType = value; }
         public State state { get; set; }
         public static bool pause = false;
-        public static bool is_first_save = true;
         public static ManualResetEvent pauseEvent = new ManualResetEvent(false);
         public static CancellationTokenSource cts = new CancellationTokenSource();
-
-
-
-
         public SaveChange SaveChangeEvent;
-
 
         public Save()
         {
@@ -60,7 +56,6 @@ namespace EasySaveApp.MVVM.Model
             SaveChangeEvent += listener;
         }
 
-
         // Get directory size
         static int GetDirectorySize(string path)
         {
@@ -73,13 +68,14 @@ namespace EasySaveApp.MVVM.Model
             }
             return (int)numberOfFileByte;
         }
+
         public static void Stop()
         {
             cts.Cancel();
             MessageBox.Show("Please wait few second");
             GC.Collect();
-
         }
+
         public static void Pause()
         {
             pause = !pause;
@@ -91,7 +87,6 @@ namespace EasySaveApp.MVVM.Model
                 {
                     MessageBox.Show("Save resumed");
                 }
-
             }
             else
             {
@@ -101,19 +96,55 @@ namespace EasySaveApp.MVVM.Model
             }
         }
 
+        public List<string> CheckPriority(string[] oldList)
+        {
+            List<string> files = new List<string>(oldList);
+            List<string> extensions = Application.Current.Properties["PriorityFiles"].ToString().Split(" ").ToList();
+            List<string> newList = new List<string>();
+            foreach (string extension in extensions)
+            {
+                newList.AddRange(files.Where(f => f.EndsWith(extension)));
+                files = files.Where(f => !f.EndsWith(extension)).ToList();
+            }
+            newList.AddRange(files);
+            return newList;
+        }
+
+        static void Crypt(string pathSource, string pathDestination)
+        {
+            if (pathDestination.Contains(".crypt"))
+            {
+                pathDestination = pathDestination.Replace(".crypt", "");
+            }
+            else
+            {
+                pathDestination += ".crypt";
+            }
+            MessageBox.Show(pathDestination);
+
+
+            string key = Application.Current.Properties["CryptKey"].ToString();
+            Process cryptosoft;
+            cryptosoft = new Process();
+            cryptosoft.StartInfo.FileName = @"../../../CryptoSoft/Cryptosoft.exe";
+            cryptosoft.StartInfo.Arguments = pathSource + " " + pathDestination + " " + key;
+            cryptosoft.StartInfo.CreateNoWindow = true;
+            cryptosoft.Start();
+            cryptosoft.WaitForExit();
+            cryptosoft.Kill();
+        }
+
         public void SaveSave()
         {
             try
             {
-                if (is_first_save)
-                {
-                    Save.Pause();
-                }
+                Pause();
                 string status = "ACTIVE";
                 string[] listOfPathFile = { };
                 int size = GetDirectorySize(PathSource);
                 Directory.CreateDirectory(PathDestination + @"\" + Name);
                 listOfPathFile = Directory.GetFiles(PathSource, "*.*", SearchOption.AllDirectories);
+                listOfPathFile = CheckPriority(listOfPathFile).ToArray();
                 int fileLeft = listOfPathFile.Length;
                 watch.Start();
 
@@ -121,12 +152,12 @@ namespace EasySaveApp.MVVM.Model
                 #region
                 foreach (string oldPath in listOfPathFile)
                 {
-                    if (cts.IsCancellationRequested)
-                    {
-                        //reset save
+                    FileInfo fileInfo = new FileInfo(oldPath);
+                    string extension = fileInfo.Extension;
+                    string[] extensionToCrypt = Application.Current.Properties["ExtensionToCrypt"].ToString().Split(" ");
 
-                        return;
-                    }
+                    if (cts.IsCancellationRequested) { return; }
+
                     pauseEvent.WaitOne();
 
                     string newPath = oldPath.Replace(PathSource, PathDestination + @"\" + Name);
@@ -138,7 +169,14 @@ namespace EasySaveApp.MVVM.Model
 
                     if (SaveType == "Complete")
                     {
-                        File.Copy(oldPath, newPath, true);
+                        if (extensionToCrypt.Contains(extension) || extension == ".crypt")
+                        {
+                            Crypt(oldPath, newPath);
+                        }
+                        else
+                        {
+                            File.Copy(oldPath, newPath, true);
+                        }
                     }
                     else if (SaveType == "Differential")
                     {
@@ -148,12 +186,26 @@ namespace EasySaveApp.MVVM.Model
                             FileInfo fileDestination = new FileInfo(newPath);
                             if (fileSource.GetHashCode() != fileDestination.GetHashCode())
                             {
-                                File.Copy(oldPath, newPath, true);
+                                if (extensionToCrypt.Contains(extension))
+                                {
+                                    Crypt(oldPath, newPath);
+                                }
+                                else
+                                {
+                                    File.Copy(oldPath, newPath, true);
+                                }
                             }
                         }
                         else
                         {
-                            File.Copy(oldPath, newPath, true);
+                            if (extensionToCrypt.Contains(extension))
+                            {
+                                Crypt(oldPath, newPath);
+                            }
+                            else
+                            {
+                                File.Copy(oldPath, newPath, true);
+                            }
                         }
                     }
 
@@ -163,6 +215,7 @@ namespace EasySaveApp.MVVM.Model
                     {
                         status = "END";
                     }
+
                     state.PathSource = PathSource;
                     state.PathDestination = PathDestination;
                     state.StateType = SaveType;
@@ -191,6 +244,7 @@ namespace EasySaveApp.MVVM.Model
                     DateTime.Now);
                 log.SaveLog(Application.Current.Properties["TypeOfLog"].ToString());
                 #endregion
+
                 is_first_save = false;
                 MainViewModel.SaveHomeViewModelCommand.Execute(null);
             }
